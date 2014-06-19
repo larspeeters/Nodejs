d3d89261c2e47a73973d18d0761f07e47d14c807
larspeeters <peeters.lars@telenet.be>
2014-06-19 16:28:27 +0200

    player 1 added

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var mongoose = require('mongoose');
 var user = require('./routes/user');
 var app = express();
 var http = require('http');
+var nicknames = [];
 
 var path = require('path');
 var mysql =  require('mysql');
@@ -50,10 +51,30 @@ app.get('/', function(req, res){
   res.render('index.jade');
 });
 
+app.get('/player1', function(req, res){
+  res.render('player1.jade');
+});
+
 io.sockets.on('connection', function(socket){
+	socket.on('new player', function(data, callback){
+		console.log(data);
+		if(nicknames.indexOf(data) != -1){
+			callback(false); //Username already in array
+		}else{
+			socket.nickname = data;
+			nicknames.push(socket.nickname);
+			io.sockets.emit('player', nicknames);			
+		}
+	});
 	socket.on('send message', function(msg){
 		io.sockets.emit('new message', msg);
 		//socket.broadcast.emit('new message', msg); //Sends msg to everyone except sender	
+	});
+	
+	socket.on('disconnect', function(data){
+		if(!socket.nickname) return; //if the user disconnects at login
+		nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+		io.sockets.emit('dc', socket.nickname);		
 	});
 });
 
